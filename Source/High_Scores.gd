2f28466603baf899609464cbd2f8b83fb93d5b60
Nathalia <nathalia.borin@usp.br>
2017-08-22 16:46:15 -0300

    Web version added

--- Source/High_Scores.gd
+++ Source/High_Scores.gd
@@ -8,7 +8,8 @@ func _ready():
 	get_child(0).set_title("High Scores")
 	get_child(0).popup()
 	scores = "  NAMES \t\t\t\t\t\tSCORES\n\n"
-	global.SCORES.sort()
+	print(global.SCORES)
+	_order(global.SCORES)
 	for i in range(2, global.SCORES.size()):
 		scores = scores + str(i-1)+" "+ global.SCORES[i][0] + "\t\t\t\t\t\t"
 		scores = scores + global.SCORES[i][1] + "\n\n"
@@ -26,4 +27,13 @@ func ok():
 func _fixed_process(delta):
 	if(Input.is_action_pressed("score")):
 		get_tree().change_scene("res://Main menu.tscn")
+		
+func _order(list):
+    for i in range(2, list.size()):
+        for j in range(2, list.size()):
+            var a = list[i]
+            var b = list[j]
+            if int(a[1]) > int(b[1]):
+                list[i] = b
+                list[j] = a
 
